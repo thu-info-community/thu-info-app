127c6fe81085f34823f3660af483aff597fa748e
UNIDY2002 <UNIDY2002@users.noreply.github.com>
2020-06-12 12:45:10 +0800

    First commit on branch `react`

---
+++ android/app/src/main/java/com/unidy2002/thuinfo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.unidy2002.thuinfo
+
+import com.facebook.react.ReactActivity
+
+class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName() = "thu_info"
+}
---
+++ app/README.md
@@ -0,0 +1,3 @@
+This directory contains the code of the original native android project.
+
+**Do not** edit if unnecessary.
