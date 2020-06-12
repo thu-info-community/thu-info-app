85a9d61163acb54e40f3abc4ccf76f0c2dc5bac5
UNIDY2002 <UNIDY2002@users.noreply.github.com>
2020-06-12 18:08:48 +0800

    Code: reformat with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+	root: true,
+	extends: "@react-native-community",
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"indent": ["error", "tab"],
+		"quotes": ["error", "double"],
+	},
 };
