7009dc3efa0b337c108d0742322f576ef95dd848
UNIDY2002 <UNIDY2002@users.noreply.github.com>
2020-07-03 22:07:45 +0800

    Function: get report

--- src/assets/translations/zh.ts
+++ src/assets/translations/zh.ts
@@ -6,4 +6,5 @@ export default {
 	news: "动态",
 	schedule: "计划",
 	report: "成绩单",
+	networkRetry: "网络异常，请重试。",
 };
---
+++ src/components/home/report.tsx
@@ -0,0 +1,100 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+const gpaToStr = (gpa: number, dig: number) =>
+	isNaN(gpa) ? "N/A" : gpa.toFixed(dig);
+
+export interface ReportHeaderProps {
+	semester: string;
+	gpa: number;
+}
+
+export const ReportHeader = (props: ReportHeaderProps) => (
+	<View style={styles.reportContainer}>
+		<Text numberOfLines={1} style={{fontSize: 18, flex: 1}}>
+			{props.semester}
+		</Text>
+		<Text numberOfLines={1} style={{fontSize: 18, flex: 0}}>
+			{gpaToStr(props.gpa, 2)}
+		</Text>
+	</View>
+);
+
+interface ReportItemProps {
+	name: string;
+	credit: number;
+	grade: string;
+	point: number;
+}
+
+const gradeToColor = {
+	"A+": "#456A2C",
+	A: "#456A2C",
+	"A-": "#456A2C",
+	"B+": "#6AA343",
+	B: "#A7B620",
+	"B-": "#BEC705",
+	"C+": "#C5A904",
+	C: "#BC820E",
+	"C-": "#B66216",
+	"D+": "#A15713",
+	D: "#74350A",
+	P: "#009242",
+	F: "#AC0000",
+};
+
+export const ReportItem = (props: ReportItemProps) => (
+	<View style={styles.reportContainer}>
+		<View
+			style={{
+				marginLeft: 4,
+				marginRight: 8,
+				alignItems: "center",
+				justifyContent: "center",
+				width: 27,
+				height: 27,
+				backgroundColor:
+					props.grade in gradeToColor ? gradeToColor[props.grade] : "#A6A6A6",
+				borderStyle: "solid",
+				borderRadius: 14,
+				paddingBottom: 2,
+			}}>
+			<Text style={{fontSize: 12, textAlign: "center", color: "#fff"}}>
+				{props.grade}
+			</Text>
+		</View>
+		<Text numberOfLines={1} style={{fontSize: 16, flex: 2}}>
+			{props.name}
+		</Text>
+		<Text
+			numberOfLines={1}
+			style={{fontSize: 16, flex: 0.2, textAlign: "right"}}>
+			{props.credit}
+		</Text>
+		<Text numberOfLines={1} style={{fontSize: 16, flex: 0}}>
+			{" · "}
+		</Text>
+		<Text
+			numberOfLines={1}
+			style={{fontSize: 16, flex: 0.3, textAlign: "left"}}>
+			{gpaToStr(props.point, 1)}
+		</Text>
+	</View>
+);
+
+export const ReportFooter = (props: {gpa: number}) => (
+	<View style={{...styles.reportContainer, justifyContent: "flex-end"}}>
+		<Text style={{fontSize: 22, textAlign: "right"}}>
+			{gpaToStr(props.gpa, 2)}
+		</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+	reportContainer: {
+		margin: 6,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+});
