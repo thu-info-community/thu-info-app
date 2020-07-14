43b0ff82c503e049e241b52bbb303e4e5433d95f
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2020-07-14 11:45:02 +0800

    Function: better login interface

--- src/assets/translations/en.ts
+++ src/assets/translations/en.ts
@@ -2,9 +2,15 @@ export default {
 	userId: "School ID",
 	password: "Password",
 	login: "Login",
+	autoLogin: "Auto-Login",
+	loggingIn: "Logging In ...",
+	loginFailure:
+		"Login failed, please check the network connection and make sure your password is correct before re-login.",
 	home: "Home",
 	news: "News",
 	schedule: "Schedule",
 	report: "Report",
 	networkRetry: "Network failure, please retry.",
+	credentialNote:
+		"Due to special reasons, your credentials are stored locally without encryption.",
 };
