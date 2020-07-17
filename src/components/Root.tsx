e716254b5adf5365e3f90f9b83744f19b73af301
UNIDY2002 <UNIDY2002@users.noreply.github.com>
2020-07-17 16:55:21 +0800

    Code: ignore unnecessary files

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,9 @@
 import {HomeStackScreen} from "../ui/home/homeStack";
 import {NewsStackScreen} from "../ui/news/newsStack";
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {connect} from "react-redux";
-import {State} from "../redux/store";
 import {fullNameThunk} from "../redux/actions/basics";
-import {defaultFullNameState} from "../redux/defaults";
-import Snackbar from "react-native-snackbar";
 import {getTicket} from "../network/core";
 import {ScheduleStackScreen} from "../ui/schedule/scheduleStack";
 import {getStr} from "../utils/i18n";
@@ -19,34 +16,16 @@ interface RootProps {
 const Tab = createBottomTabNavigator();
 
 const RootComponent = (props: RootProps) => {
-	const {fullName} = props;
-	const [firstTime, setFirstTime] = useState(true);
-
 	useEffect(() => {
-		if (firstTime) {
-			setFirstTime(false);
-			props.getFullName();
+		// Things that should be done only once upon logged in
 
-			// Things that should be done only once upon logged in
+		props.getFullName();
 
-			// Important network operations
-			getTicket(792).then(() => console.log("Ticket 792 get."));
-			getTicket(824).then(() => console.log("Ticket 824 get."));
-			getTicket(2005).then(() => console.log("Ticket 2005 get."));
-		}
+		getTicket(792).then(() => console.log("Ticket 792 get."));
+		getTicket(824).then(() => console.log("Ticket 824 get."));
+		getTicket(2005).then(() => console.log("Ticket 2005 get."));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [firstTime]);
-
-	// In production, the following part shall be removed as it damages
-	// user experience.
-	useEffect(() => {
-		if (fullName !== defaultFullNameState) {
-			Snackbar.show({
-				text: `欢迎，${fullName}！`,
-				duration: Snackbar.LENGTH_SHORT,
-			});
-		}
-	}, [fullName]);
+	}, []);
 
 	return (
 		<Tab.Navigator>
@@ -69,16 +48,11 @@ const RootComponent = (props: RootProps) => {
 	);
 };
 
-export const Root = connect(
-	(state: State) => {
-		return {fullName: state.fullName};
-	},
-	(dispatch) => {
-		return {
-			getFullName: () => {
-				// @ts-ignore
-				dispatch(fullNameThunk());
-			},
-		};
-	},
-)(RootComponent);
+export const Root = connect(undefined, (dispatch) => {
+	return {
+		getFullName: () => {
+			// @ts-ignore
+			dispatch(fullNameThunk());
+		},
+	};
+})(RootComponent);
