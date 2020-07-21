2e41c98a611db8bd04501d9d77ad29bbf523c5e2
UNIDY2002 <UNIDY2002@outlook.com>
2020-07-21 10:30:51 +0800

    Function: schedule (parse primary & exam list from internet)

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -10,10 +10,12 @@ declare global {
 	}
 }
 
+// eslint-disable-next-line no-extend-native
 Array.prototype.flatMap = function (transform: (item: any) => any[]) {
 	return this.reduce((prev, curr) => prev.concat(transform(curr)), []);
 };
 
+// eslint-disable-next-line no-extend-native
 Map.prototype.putAll = function (pairs: [any, any][]) {
 	pairs.forEach(([key, value]) => this.set(key, value));
 	return this;
