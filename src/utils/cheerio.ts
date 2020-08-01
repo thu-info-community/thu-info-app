0847c09ccf9a55f962ad4df3f967b7f5fcac64a0
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2020-08-01 16:16:23 +0800

    Function: better navigation

--- src/utils/cheerio.ts
+++ src/utils/cheerio.ts
@@ -1,4 +1,4 @@
 export const getCheerioText = (element: CheerioElement, index?: number) =>
 	index === undefined
-		? element.firstChild.data?.trim()
-		: element.children[index].firstChild.data?.trim();
+		? element.firstChild?.data?.trim() ?? ""
+		: element.children[index].firstChild?.data?.trim() ?? "";
