2756110f17d9759b87d472a262f67122df973ac2
UNIDY2002 <UNIDY2002@outlook.com>
2020-08-22 11:36:43 +0800

    Improvement: set icons for home page buttons

---
+++ src/utils/svgGenerator.ts
@@ -0,0 +1,24 @@
+interface ColorBoard {
+	dark: string;
+	green: string;
+	yellow: string;
+	blue: string;
+	golden: string;
+	red: string;
+}
+
+export const svgGenerator = (
+	transform: (
+		width: number | string | undefined,
+		height: number | string | undefined,
+		colors: ColorBoard,
+	) => JSX.Element,
+) => ({width, height}: {width?: number | string; height?: number | string}) =>
+	transform(width, height, {
+		dark: "#202020",
+		green: "#008577",
+		yellow: "#FF9800",
+		blue: "#2196F3",
+		golden: "#F1DA71",
+		red: "#E53935",
+	});
