fc7a365295cb837144f79a566efcf4fcec73e28a
UNIDY2002 <UNIDY2002@outlook.com>
2020-08-23 10:12:07 +0800

    Improvement: do keep-alive

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,43 +1,18 @@
 import {HomeStackScreen} from "../ui/home/homeStack";
 import {NewsStackScreen} from "../ui/news/newsStack";
-import React, {useContext, useEffect} from "react";
+import React, {useContext} from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {connect} from "react-redux";
-import {fullNameThunk} from "../redux/actions/basics";
-import {getTicket} from "../network/core";
 import {ScheduleStackScreen} from "../ui/schedule/scheduleStack";
 import {getStr} from "../utils/i18n";
 import Icon from "react-native-vector-icons/FontAwesome";
 import themes from "../assets/themes/themes";
 import {ThemeContext} from "../assets/themes/context";
-import {checkUpdate} from "../utils/checkUpdate";
-import {leanCloudInit} from "../utils/leanCloud";
 import {SettingStackScreen} from "../ui/settings/settingsStack";
-
-interface RootProps {
-	getFullName: () => void;
-}
 
 const Tab = createBottomTabNavigator();
 
-const RootComponent = (props: RootProps) => {
+export const Root = () => {
 	const theme = themes[useContext(ThemeContext)];
-
-	useEffect(() => {
-		// Things that should be done only once upon logged in
-
-		props.getFullName();
-
-		getTicket(792).then(() => console.log("Ticket 792 get."));
-		getTicket(824).then(() => console.log("Ticket 824 get."));
-		getTicket(2005).then(() => console.log("Ticket 2005 get."));
-		getTicket(-1).then(() => console.log("Ticket -1 get."));
-
-		checkUpdate();
-
-		leanCloudInit();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
 
 	return (
 		<Tab.Navigator
@@ -94,12 +69,3 @@ const RootComponent = (props: RootProps) => {
 		</Tab.Navigator>
 	);
 };
-
-export const Root = connect(undefined, (dispatch) => {
-	return {
-		getFullName: () => {
-			// @ts-ignore
-			dispatch(fullNameThunk());
-		},
-	};
-})(RootComponent);
