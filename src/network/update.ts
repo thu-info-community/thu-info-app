7cb2d081ea156be98e6245e9a91a2066c48cd9c0
UNIDY2002 <UNIDY2002@outlook.com>
2020-09-17 23:02:49 +0800

    Code: mock update.ts

--- src/network/update.ts
+++ src/network/update.ts
@@ -1,6 +1,7 @@
 import {retrieve} from "./core";
 import {TSINGHUA_CLOUD_URL, UPDATE_INFO_URL} from "../constants/strings";
 import VersionNumber from "react-native-version-number";
+import {mocked} from "../redux/store";
 
 interface UpdateInfo {
 	versionCode: number;
@@ -10,10 +11,12 @@ interface UpdateInfo {
 }
 
 export const getUpdateInfo = () =>
-	retrieve(UPDATE_INFO_URL, TSINGHUA_CLOUD_URL)
-		.then(JSON.parse)
-		.then((o: UpdateInfo[]) =>
-			o
-				.filter((it) => it.versionCode > Number(VersionNumber.buildVersion))
-				.sort((a, b) => b.versionCode - a.versionCode),
-		);
+	mocked()
+		? Promise.resolve([])
+		: retrieve(UPDATE_INFO_URL, TSINGHUA_CLOUD_URL)
+				.then(JSON.parse)
+				.then((o: UpdateInfo[]) =>
+					o
+						.filter((it) => it.versionCode > Number(VersionNumber.buildVersion))
+						.sort((a, b) => b.versionCode - a.versionCode),
+				);
