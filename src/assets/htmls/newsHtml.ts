57fdf4151bce64260a7cbb3ce124f652cd33221e
UNIDY2002 <UNIDY2002@outlook.com>
2020-09-18 22:44:55 +0800

    Code: mock all news html

--- src/assets/htmls/newsHtml.ts
+++ src/assets/htmls/newsHtml.ts
@@ -5,6 +5,14 @@ const prevalData = preval`
     const path = require('path');
     const url0 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e9fd528569336153301c9aa596522b20735d12f268e561f0/boarddetail_cat.jsp?columnId=0010106&itemSeq=46224";
     const url1 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fae0429e207e6b597d469dbf915b243d8ae9128e1cdcffb247/jwcbg/detail_cat.jsp?boardid=57&seq=7632";
+    const url2 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fae0429e207e6b597d469dbf915b243d8ae9128e1cdcffb247/jwcbg/detail_cat.jsp?boardid=57&seq=7631";
+    const url3 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fae0429e207e6b597d469dbf915b243d8ae9128e1cdcffb247/jwcbg/detail_cat.jsp?boardid=57&seq=7627";
+    const url4 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=xtw01&itemSeq=124715";
+    const url5 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=xtw01&itemSeq=124712";
+    const url6 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e2e442d23323615e79009cadd650272001f8dd/rscbg/detail.jsp?boardid=22&seq=5182";
+    const url7 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e0f852882e3e6e5f301c9aa596522b2043f84ba24ebecaf8/node/279771";
+    const url8 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=dwxcb01&itemSeq=124530";
+    const url9 = "https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e0f852882e3e6e5f301c9aa596522b2043f84ba24ebecaf8/node/279661";
     const result = {};
     let flag = false;
     if (process.cwd().includes("ios")) {
@@ -13,20 +21,54 @@ const prevalData = preval`
     }
     result[url0] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/0.html'), 'utf8');
     result[url1] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/1.html'), 'utf8');
+    result[url2] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/2.html'), 'utf8');
+    result[url3] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/3.html'), 'utf8');
+    result[url4] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/4.html'), 'utf8');
+    result[url5] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/5.html'), 'utf8');
+    result[url6] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/6.html'), 'utf8');
+    result[url7] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/7.html'), 'utf8');
+    result[url8] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/8.html'), 'utf8');
+    result[url9] = fs.readFileSync(path.resolve(process.cwd(), './src/assets/htmls/9.html'), 'utf8');
     if (flag) {
         process.chdir('ios')
     }
     module.exports = {
         url0,
         url1,
+        url2,
+        url3,
+        url4,
+        url5,
+        url6,
+        url7,
+        url8,
+        url9,
         newsHtml: result,
     };
 ` as {
 	newsHtml: {[key: string]: string};
 	url0: string;
 	url1: string;
+	url2: string;
+	url3: string;
+	url4: string;
+	url5: string;
+	url6: string;
+	url7: string;
+	url8: string;
+	url9: string;
 };
 
-export const newsHtml = prevalData.newsHtml;
-export const url0 = prevalData.url0;
-export const url1 = prevalData.url1;
+export const {
+	newsHtml,
+	url0,
+	url1,
+	url2,
+	url3,
+	url4,
+	url5,
+	url6,
+	url7,
+	url8,
+	url9,
+} = prevalData;
--- src/network/news.ts
+++ src/network/news.ts
@@ -6,9 +6,20 @@ import {
 	BGTZ_MAIN_PREFIX,
 	HB_MAIN_PREFIX,
 	JWGG_MAIN_PREFIX,
-	KYTZ_MAIN_PREFIX,
 } from "../constants/strings";
-import {newsHtml, url0, url1} from "../assets/htmls/newsHtml";
+import {
+	newsHtml,
+	url0,
+	url1,
+	url2,
+	url3,
+	url4,
+	url5,
+	url6,
+	url7,
+	url8,
+	url9,
+} from "../assets/htmls/newsHtml";
 
 export type sourceTag = "JWGG" | "KYTZ" | "BGTZ" | "HB";
 
@@ -49,16 +60,14 @@ export const getNewsList = (
 						date: "2020.09.14",
 						name: "本科生补退选时间安排（2020-2021秋）",
 						source: "教务处",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fae0429e207e6b597d469dbf915b243d8ae9128e1cdcffb247/jwcbg/detail_cat.jsp?boardid=57&seq=7631",
+						url: url2,
 					},
 					{
 						channel: "JWGG",
 						date: "2020.09.14",
 						name: "2020-2021学年秋季学期SRT立项通知",
 						source: "教务处",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fae0429e207e6b597d469dbf915b243d8ae9128e1cdcffb247/jwcbg/detail_cat.jsp?boardid=57&seq=7627",
+						url: url3,
 					},
 				]);
 			case BGTZ_MAIN_PREFIX + 0:
@@ -68,24 +77,21 @@ export const getNewsList = (
 						date: "2020.09.17",
 						name: "第二十二届清华大学创业大赛参赛报名通知",
 						source: "校团委",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=xtw01&itemSeq=124715",
+						url: url4,
 					},
 					{
 						channel: "BGTZ",
 						date: "2020.09.17",
 						name: "清华大学“启·创”学生创业人才培育计划八期公开招募通知",
 						source: "校团委",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=xtw01&itemSeq=124712",
+						url: url5,
 					},
 					{
 						channel: "BGTZ",
 						date: "2020.09.17",
 						name: "关于召开2020年度人事工作会的通知",
 						source: "人事处",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e2e442d23323615e79009cadd650272001f8dd/rscbg/detail.jsp?boardid=22&seq=5182",
+						url: url6,
 					},
 				]);
 			case HB_MAIN_PREFIX + 0:
@@ -95,8 +101,7 @@ export const getNewsList = (
 						date: "2020.09.17",
 						name: "2020年10月三场“LIVE 逍遥夜”演出开票公告",
 						source: "艺教中心办公室",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e0f852882e3e6e5f301c9aa596522b2043f84ba24ebecaf8/node/279771",
+						url: url7,
 					},
 					{
 						channel: "HB",
@@ -104,8 +109,7 @@ export const getNewsList = (
 						name:
 							"清华大学“唯真讲坛”系列在线理论宣讲活动第二十讲 当前的国际形势与热点问题",
 						source: "党委宣传部",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421f2fa598421322653770bc7b88b5c2d32530b094045c3bd5cabf3/boarddetail_cat.jsp?columnId=dwxcb01&itemSeq=124530",
+						url: url8,
 					},
 					{
 						channel: "HB",
@@ -113,27 +117,7 @@ export const getNewsList = (
 						name:
 							"金融科技还是科技金融？——数字刀如何切开金融蛋糕|清华五道口云课堂之金融大家评·华尔街热线（第15期）",
 						source: "五道口金融学院综合办公室",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421e0f852882e3e6e5f301c9aa596522b2043f84ba24ebecaf8/node/279661",
-					},
-				]);
-			case KYTZ_MAIN_PREFIX + 0:
-				return Promise.resolve([
-					{
-						channel: "KYTZ",
-						date: "2020.09.17",
-						name: "转发：国家知识产权局关于评选第二十二届中国专利奖的通知",
-						source: "科研院",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fbee589e2028701e7d018ae28c462a3be90b228bbda6767746aeb6/kybg/detail.jsp?boardid=34&seq=31631",
-					},
-					{
-						channel: "KYTZ",
-						date: "2020.09.11",
-						name: "清华大学国际大科学项目培育计划预研课题立项公示",
-						source: "科研院",
-						url:
-							"https://webvpn.tsinghua.edu.cn/http/77726476706e69737468656265737421fbee589e2028701e7d018ae28c462a3be90b228bbda6767746aeb6/kybg/detail.jsp?boardid=34&seq=31604",
+						url: url9,
 					},
 				]);
 			default:
