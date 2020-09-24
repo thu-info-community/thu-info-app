7a8209872a9653ae77fbdb7c0fe1d3f1c3d7cd7c
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2020-09-24 17:13:11 +0800

    Function: reset remainder ui update

--- src/assets/translations/en.ts
+++ src/assets/translations/en.ts
@@ -95,7 +95,7 @@ export default {
 	remainder: "Remainder",
 	question: "FAQ",
 	expenditureFAQ:
-		"\nRemainder does not match the difference between income and expenditure?\n\nThe income and expenditure are calculated based on your selected range of dates while remainder is based on the whole history of your expenditure records.\n\nRemainder does not match the actual value of my card?\n\nAs no direct API has been found yet, the remainder value is deduced based on the whole history of your expenditure records, and might be incorrect. Moreover, it seems that if you have lost your card and made a new one before, there can be serious deviation of the expenditure record. Please reset your remainder manually in the settings.",
+		"\nRemainder does not match the difference between income and expenditure?\n\nThe income and expenditure are calculated based on your selected range of dates while remainder is based on the whole history of your expenditure records.\n\nRemainder does not match the actual value of my card?\n\nAs no direct API has been found yet, the remainder value is deduced based on the whole history of your expenditure records, and might be incorrect. Moreover, it seems that if you have lost your card and made a new one before, there can be serious deviation of the expenditure record. In this case, please reset your remainder manually in the settings.",
 	classroomState: "Classrooms",
 	classroomStatus: ["Teaching", "Exam", "Borrowed", "Disabled", "Unknown"],
 	classroomHeaderPrefix: "Week ",
--- src/ui/settings/remainderSettings.tsx
+++ src/ui/settings/remainderSettings.tsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import {getStr} from "../../utils/i18n";
 import {SET_REMAINDER_SHIFT} from "../../redux/constants";
 import {connect} from "react-redux";
-import {Button, View} from "react-native";
+import {Button, View, Text} from "react-native";
 import {getExpenditures} from "../../network/basics";
 import Snackbar from "react-native-snackbar";
 import {NetworkRetry} from "../../components/easySnackbars";
@@ -22,24 +22,49 @@ export const RemainderSettingsUI = ({
 				value={newValue}
 				onValueChange={setNewValue}
 			/>
-			<Button
-				title={getStr("confirm")}
-				onPress={() => {
-					Snackbar.show({
-						text: getStr("processing"),
-						duration: Snackbar.LENGTH_SHORT,
-					});
-					getExpenditures(new Date(), new Date())
-						.then((r) => {
-							setRemainderShift(newValue - r[3]);
-							Snackbar.show({
-								text: getStr("success"),
-								duration: Snackbar.LENGTH_SHORT,
-							});
-						})
-						.catch(NetworkRetry);
-				}}
-			/>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-around",
+					alignItems: "center",
+				}}>
+				<Button
+					title={getStr("confirm")}
+					onPress={() => {
+						Snackbar.show({
+							text: getStr("processing"),
+							duration: Snackbar.LENGTH_SHORT,
+						});
+						getExpenditures(new Date(), new Date())
+							.then((r) => {
+								setRemainderShift(newValue - r[3]);
+								Snackbar.show({
+									text: getStr("success"),
+									duration: Snackbar.LENGTH_SHORT,
+								});
+							})
+							.catch(NetworkRetry);
+					}}
+				/>
+				<Button
+					title={getStr("resetRemainderShift")}
+					onPress={() => {
+						setRemainderShift(0);
+						Snackbar.show({
+							text: getStr("success"),
+							duration: Snackbar.LENGTH_SHORT,
+						});
+					}}
+				/>
+			</View>
+			<Text style={{padding: 10, marginTop: 10, textAlign: "center"}}>
+				<Text style={{fontWeight: "bold", fontSize: 16, lineHeight: 18}}>
+					{getStr("tips")}
+				</Text>
+				<Text style={{color: "gray", lineHeight: 18}}>
+					{getStr("remainderTips")}
+				</Text>
+			</Text>
 		</View>
 	);
 };
