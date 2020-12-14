ff96fd624c7f50882d848b1b0235f419cbc1e9a8
UNIDY2002 <UNIDY2002@outlook.com>
2020-12-14 17:01:14 +0800

    Improvement: open up ele recharge (in experimental page)

--- src/ui/settings/experimental.tsx
+++ src/ui/settings/experimental.tsx
@@ -1,10 +1,14 @@
-import {SettingsItem} from "../../components/settings/items";
+import {SettingsItem, SettingsSeparator} from "../../components/settings/items";
 import {getStr} from "../../utils/i18n";
 import React from "react";
 import Feather from "react-native-vector-icons/Feather";
 import {Text, View} from "react-native";
 import {performLoseCard} from "../../components/home/loseCard";
 import {SettingsNav} from "./settingsStack";
+import {mocked} from "../../redux/store";
+import {AlipayPopup} from "../../components/home/alipayPopup";
+import {getEleRechargePayCode} from "../../network/dorm";
+import {doAlipay} from "../../utils/alipay";
 
 export const ExperimentalScreen = ({navigation}: {navigation: SettingsNav}) => (
 	<View style={{padding: 10}}>
@@ -16,7 +20,8 @@ export const ExperimentalScreen = ({navigation}: {navigation: SettingsNav}) => (
 			onPress={performLoseCard}
 			icon={<Feather name="credit-card" size={16} />}
 		/>
-		{/*!mocked() && (
+		<SettingsSeparator />
+		{!mocked() && (
 			<AlipayPopup
 				onPay={(money) => getEleRechargePayCode(money).then(doAlipay)}
 				trigger={(onPress) => (
@@ -27,7 +32,7 @@ export const ExperimentalScreen = ({navigation}: {navigation: SettingsNav}) => (
 					/>
 				)}
 			/>
-		)*/}
+		)}
 		<SettingsItem
 			text={getStr("eleRecord")}
 			onPress={() => navigation.navigate("EleRecord")}
