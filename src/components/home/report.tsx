a81c584f636f3aa6992110366b8dc2ecd6edb5ca
UNIDY2002 <UNIDY2002@outlook.com>
2021-01-29 20:01:38 +0800

    Improvement: fix report page dark mode color

--- src/components/home/report.tsx
+++ src/components/home/report.tsx
@@ -1,6 +1,8 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Text, View} from "react-native";
 import React from "react";
 import {currState} from "../../redux/store";
+import themedStyles from "../../utils/themedStyles";
+import {useColorScheme} from "react-native-appearance";
 
 const gpaToStr = (gpa: number, dig: number) =>
 	isNaN(gpa) ? "N/A" : gpa.toFixed(dig);
@@ -10,17 +12,25 @@ export interface ReportHeaderProps {
 	gpa: number;
 }
 
-export const ReportHeader = (props: ReportHeaderProps) => (
-	<View
-		style={{...styles.reportContainer, padding: 10, backgroundColor: "white"}}>
-		<Text numberOfLines={1} style={{fontSize: 18, flex: 1}}>
-			{props.semester}
-		</Text>
-		<Text numberOfLines={1} style={{fontSize: 18, flex: 0}}>
-			{gpaToStr(props.gpa, 3)}
-		</Text>
-	</View>
-);
+export const ReportHeader = (props: ReportHeaderProps) => {
+	const themeName = useColorScheme();
+	const style = styles(themeName);
+
+	return (
+		<View style={[style.reportContainer, {padding: 10}]}>
+			<Text
+				numberOfLines={1}
+				style={[style.reportText, {fontSize: 18, flex: 1}]}>
+				{props.semester}
+			</Text>
+			<Text
+				numberOfLines={1}
+				style={[style.reportText, {fontSize: 18, flex: 0}]}>
+				{gpaToStr(props.gpa, 3)}
+			</Text>
+		</View>
+	);
+};
 
 interface ReportItemProps {
 	name: string;
@@ -45,62 +55,91 @@ const gradeToColor: {[key: string]: string} = {
 	F: "#AC0000",
 };
 
-export const ReportItem = (props: ReportItemProps) => (
-	<View style={styles.reportContainer}>
-		<View
-			style={{
-				marginLeft: 4,
-				marginRight: 8,
-				alignItems: "center",
-				justifyContent: "center",
-				width: 27,
-				height: 27,
-				backgroundColor:
-					props.grade === "A-" && !currState().config.newGPA
-						? "#5E943D"
-						: props.grade in gradeToColor
-						? gradeToColor[props.grade]
-						: "#A6A6A6",
-				borderStyle: "solid",
-				borderRadius: 14,
-				paddingBottom: 2,
-			}}>
-			<Text style={{fontSize: 12, textAlign: "center", color: "#fff"}}>
-				{props.grade}
+export const ReportItem = (props: ReportItemProps) => {
+	const themeName = useColorScheme();
+	const style = styles(themeName);
+
+	return (
+		<View style={style.reportContainer}>
+			<View
+				style={[
+					style.reportItem,
+					{
+						backgroundColor:
+							props.grade === "A-" && !currState().config.newGPA
+								? "#5E943D"
+								: props.grade in gradeToColor
+								? gradeToColor[props.grade]
+								: "#A6A6A6",
+					},
+				]}>
+				<Text style={{fontSize: 12, textAlign: "center", color: "#fff"}}>
+					{props.grade}
+				</Text>
+			</View>
+			<Text
+				numberOfLines={1}
+				style={[style.reportText, {fontSize: 16, flex: 2}]}>
+				{props.name}
+			</Text>
+			<Text
+				numberOfLines={1}
+				style={[
+					style.reportText,
+					{fontSize: 16, flex: 0.2, textAlign: "right"},
+				]}>
+				{props.credit}
+			</Text>
+			<Text
+				numberOfLines={1}
+				style={[style.reportText, {fontSize: 16, flex: 0}]}>
+				{" · "}
+			</Text>
+			<Text
+				numberOfLines={1}
+				style={[
+					style.reportText,
+					{fontSize: 16, flex: 0.3, textAlign: "left"},
+				]}>
+				{gpaToStr(props.point, 1)}
 			</Text>
 		</View>
-		<Text numberOfLines={1} style={{fontSize: 16, flex: 2}}>
-			{props.name}
-		</Text>
-		<Text
-			numberOfLines={1}
-			style={{fontSize: 16, flex: 0.2, textAlign: "right"}}>
-			{props.credit}
-		</Text>
-		<Text numberOfLines={1} style={{fontSize: 16, flex: 0}}>
-			{" · "}
-		</Text>
-		<Text
-			numberOfLines={1}
-			style={{fontSize: 16, flex: 0.3, textAlign: "left"}}>
-			{gpaToStr(props.point, 1)}
-		</Text>
-	</View>
-);
+	);
+};
 
-export const ReportFooter = (props: {gpa: number}) => (
-	<View style={{...styles.reportContainer, justifyContent: "flex-end"}}>
-		<Text style={{fontSize: 20, textAlign: "right"}}>
-			{gpaToStr(props.gpa, 3)}
-		</Text>
-	</View>
-);
+export const ReportFooter = (props: {gpa: number}) => {
+	const themeName = useColorScheme();
+	const style = styles(themeName);
 
-const styles = StyleSheet.create({
+	return (
+		<View style={{...style.reportContainer, justifyContent: "flex-end"}}>
+			<Text style={[style.reportText, {fontSize: 20, textAlign: "right"}]}>
+				{gpaToStr(props.gpa, 3)}
+			</Text>
+		</View>
+	);
+};
+
+const styles = themedStyles((theme) => ({
 	reportContainer: {
 		padding: 6,
 		flexDirection: "row",
 		justifyContent: "space-between",
 		alignItems: "center",
+		backgroundColor: theme.colors.background,
 	},
-});
+	reportItem: {
+		marginLeft: 4,
+		marginRight: 8,
+		alignItems: "center",
+		justifyContent: "center",
+		width: 27,
+		height: 27,
+		borderStyle: "solid",
+		borderRadius: 14,
+		paddingBottom: 2,
+	},
+	reportText: {
+		color: theme.colors.text,
+	},
+}));
