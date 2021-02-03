2dc79fe7e5dc63e24fb99096418b1dc98a5973fa
UNIDY2002 <UNIDY2002@outlook.com>
2021-02-03 18:37:39 +0800

    Chore: add debug info for dormScore.tsx

--- src/ui/home/dormScore.tsx
+++ src/ui/home/dormScore.tsx
@@ -1,15 +1,50 @@
-import {View} from "react-native";
+import {View, Text, ScrollView} from "react-native";
 import React, {useEffect, useState} from "react";
 import Snackbar from "react-native-snackbar";
 import {getStr} from "../../utils/i18n";
 import ImageViewer from "react-native-image-zoom-viewer";
 import {helper} from "../../redux/store";
+import {retryWrapper} from "thu-info-lib/lib/lib/core";
+import {retrieve} from "thu-info-lib/lib/utils/network";
+import {
+	DORM_SCORE_HOST,
+	DORM_SCORE_REFERER,
+	DORM_SCORE_URL,
+} from "thu-info-lib/lib/constants/strings";
+import cheerio from "cheerio";
 
 export const DormScoreScreen = () => {
 	const [url, setUrl] = useState<string>();
+	const [debug, setDebug] = useState<string>();
+
 	useEffect(() => {
-		helper
-			.getDormScore()
+		retryWrapper(
+			helper,
+			-1,
+			retrieve(DORM_SCORE_URL, DORM_SCORE_REFERER).then((s) => {
+				try {
+					setDebug(
+						`[Debug] Element HTML: ${cheerio(
+							"#weixin_health_linechartCtrl1_Chart1",
+							s,
+						)}}
+src: ${cheerio("#weixin_health_linechartCtrl1_Chart1", s).attr().src}`,
+					);
+				} catch (e) {
+					setDebug(
+						`[Debug] Element HTML: ${cheerio(
+							"#weixin_health_linechartCtrl1_Chart1",
+							s,
+						)}}
+Err: ${e}`,
+					);
+				}
+				return (
+					DORM_SCORE_HOST +
+					cheerio("#weixin_health_linechartCtrl1_Chart1", s).attr().src
+				);
+			}),
+		)
 			.then(setUrl)
 			.catch(() =>
 				Snackbar.show({
@@ -19,6 +54,11 @@ export const DormScoreScreen = () => {
 			);
 	}, []);
 	return (
-		<View style={{flex: 1}}>{url && <ImageViewer imageUrls={[{url}]} />}</View>
+		<View style={{flex: 1}}>
+			{url && <ImageViewer imageUrls={[{url}]} style={{flex: 1}} />}
+			<ScrollView style={{flex: 1}}>
+				<Text>{debug}</Text>
+			</ScrollView>
+		</View>
 	);
 };
