3db6d71c5a3e6b499b8da5497e7933ba1d88f9cd
UNIDY2002 <UNIDY2002@outlook.com>
2021-05-03 21:47:33 +0800

    Chore: remove flipper

--- android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
+++ android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
@@ -1,14 +1,11 @@
 package com.unidy2002.thuinfo
 
 import android.app.Application
-import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.swmansion.reanimated.ReanimatedJSIModulePackage
-import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
     private val mReactNativeHost = object : ReactNativeHost(this) {
@@ -26,35 +23,5 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
-        initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    }
-
-    companion object {
-        /**
-         * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-         * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-         *
-         * @param context
-         * @param reactInstanceManager
-         */
-        private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
-            if (BuildConfig.DEBUG) {
-                try {
-                    // We use reflection here to pick up the class that initializes Flipper,
-                    // since Flipper library is not available in release mode
-                    Class.forName("com.thu_info.ReactNativeFlipper")
-                            .getMethod("initializeFlipper", Context::class.java, ReactInstanceManager::class.java)
-                            .invoke(null, context, reactInstanceManager)
-                } catch (e: ClassNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: NoSuchMethodException) {
-                    e.printStackTrace()
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                } catch (e: InvocationTargetException) {
-                    e.printStackTrace()
-                }
-            }
-        }
     }
 }
