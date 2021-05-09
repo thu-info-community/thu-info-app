1eb719d4c19e7df5c6bff3cf8f08f5f24ff0ed34
UNIDY2002 <UNIDY2002@outlook.com>
2021-05-09 13:12:58 +0800

    Feat: add email feedback anchors

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,6 +2,7 @@ import MailCore from "thu-info-mailcore";
 import {helper, store} from "../redux/store";
 import {Platform} from "react-native";
 import {SET_EMAIL_UNSEEN} from "../redux/constants";
+import AV from "leancloud-storage/core";
 
 const INBOX = "INBOX";
 
@@ -9,18 +10,30 @@ export const emailInit = async () => {
 	if (Platform.OS === "ios") {
 		return;
 	}
-	await MailCore.loginImap({
-		hostname: "mails.tsinghua.edu.cn",
-		port: 993, // port for smtp is 465
-		username: `${helper.emailName}@mails.tsinghua.edu.cn`,
-		password: helper.password,
-		authType: 0,
-	});
-	const {unseenCount} = await MailCore.statusFolder({folder: INBOX});
-	store.dispatch({type: SET_EMAIL_UNSEEN, payload: unseenCount});
+	try {
+		await MailCore.loginImap({
+			hostname: "mails.tsinghua.edu.cn",
+			port: 993, // port for smtp is 465
+			username: `${helper.emailName}@mails.tsinghua.edu.cn`,
+			password: helper.password,
+			authType: 0,
+		});
+		const {unseenCount} = await MailCore.statusFolder({folder: INBOX});
+		store.dispatch({type: SET_EMAIL_UNSEEN, payload: unseenCount});
+	} catch (e) {
+		const feedback = new (AV.Object.extend("EmailFeedback"))();
+		feedback.set("e", String(e));
+		feedback.save().then(console.log);
+		throw e;
+	}
 };
 
 export const getMails = () =>
-	MailCore.getMails({folder: INBOX, requestKind: 63}).then(({mails}) =>
-		mails.reverse(),
-	);
+	MailCore.getMails({folder: INBOX, requestKind: 63})
+		.then(({mails}) => mails.reverse())
+		.catch((e) => {
+			const feedback = new (AV.Object.extend("EmailFeedback"))();
+			feedback.set("e", String(e));
+			feedback.save().then(console.log);
+			throw e;
+		});
