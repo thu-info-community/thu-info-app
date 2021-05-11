6fb12c86cb84bbfe9db8b6576d91b855f120ffad
UNIDY2002 <UNIDY2002@outlook.com>
2021-05-12 00:16:50 +0800

    Chore: deprecate `react-native-popup-menu`

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,28 @@ import {PersistGate} from "redux-persist/integration/react";
 import React from "react";
 import {AuthFlow} from "./components/AuthFlow";
 import {DefaultTheme, NavigationContainer} from "@react-navigation/native";
-import {MenuProvider} from "react-native-popup-menu";
 import {useColorScheme} from "react-native";
 import themes from "./assets/themes/themes";
 
 export const App = () => (
 	<Provider store={store}>
-		<MenuProvider>
-			<PersistGate persistor={persistor}>
-				<NavigationContainer
-					theme={{
-						...DefaultTheme,
-						colors: {
-							...DefaultTheme.colors,
-							text: themes(useColorScheme()).colors.text,
-							background: themes(useColorScheme()).colors.background,
-							card: themes(useColorScheme()).colors.background,
-							border:
-								useColorScheme() === "dark"
-									? "white"
-									: DefaultTheme.colors.border,
-						},
-					}}>
-					<AuthFlow />
-				</NavigationContainer>
-			</PersistGate>
-		</MenuProvider>
+		<PersistGate persistor={persistor}>
+			<NavigationContainer
+				theme={{
+					...DefaultTheme,
+					colors: {
+						...DefaultTheme.colors,
+						text: themes(useColorScheme()).colors.text,
+						background: themes(useColorScheme()).colors.background,
+						card: themes(useColorScheme()).colors.background,
+						border:
+							useColorScheme() === "dark"
+								? "white"
+								: DefaultTheme.colors.border,
+					},
+				}}>
+				<AuthFlow />
+			</NavigationContainer>
+		</PersistGate>
 	</Provider>
 );
