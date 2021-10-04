8b76e4343ec0489bc038a7d23ddbf90918db75d2
UNIDY2002 <UNIDY2002@outlook.com>
2021-10-05 00:45:50 +0800

    fix: fix phone number input issue regarding sports detail

--- src/ui/home/sportsDetail.tsx
+++ src/ui/home/sportsDetail.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {HomeNav, SportsDetailProp} from "./homeStack";
-import {simpleRefreshListScreen} from "../../components/settings/simpleRefreshListScreen";
 import {helper} from "../../redux/store";
 import {
 	Alert,
+	FlatList,
+	RefreshControl,
 	Text,
 	TextInput,
 	TouchableOpacity,
@@ -13,6 +14,7 @@ import {
 import {getStr} from "../../utils/i18n";
 import Snackbar from "react-native-snackbar";
 import themes from "../../assets/themes/themes";
+import {NetworkRetry} from "../../components/easySnackbars";
 
 interface TimePeriod {
 	description: string;
@@ -29,95 +31,74 @@ export const SportsDetailScreen = ({
 }) => {
 	const [phoneNumber, setPhoneNumber] = useState<string | undefined>(undefined);
 	const [phoneInput, setPhoneInput] = useState("");
+	const [resources, setResources] = useState<TimePeriod[]>([]);
+	const [refreshing, setRefreshing] = useState(false);
 
 	const themeName = useColorScheme();
 	const {colors} = themes(themeName);
 
-	const RefreshList = simpleRefreshListScreen(
-		() =>
-			helper
-				.getSportsResources(
-					route.params.info.gymId,
-					route.params.info.itemId,
-					route.params.date,
-				)
-				.then(({data, init, count, phone}) => {
-					setPhoneInput(phone ?? "");
-					setPhoneNumber(phone);
-					if (init <= 0) {
-						Alert.alert(getStr("failure"), getStr("sportsBookUnavailable"));
-						return [];
-					} else if (count === 0) {
-						Alert.alert(
-							getStr("failure"),
-							getStr("sportsBookRestricted").replace("%d", String(init)),
-						);
-						return [];
-					} else if (phone === undefined) {
-						Alert.alert(getStr("sportsBookPhoneRequired"));
-						return [];
-					} else {
-						const result: {[key: string]: TimePeriod} = {};
-						for (let {
-							timeSession,
-							locked,
-							userType,
-							resId,
-							fieldName,
-							cost,
-						} of data) {
-							if (result[timeSession] === undefined) {
-								result[timeSession] = {
-									description: timeSession,
-									total: 0,
-									availableFields: [],
-								};
-							}
-							++result[timeSession].total;
-							if (locked !== true && userType === undefined) {
-								result[timeSession].availableFields.push({
-									id: resId,
-									name: fieldName,
-									cost: cost ?? 0,
-								});
-							}
+	const refresh = () => {
+		setRefreshing(true);
+		helper
+			.getSportsResources(
+				route.params.info.gymId,
+				route.params.info.itemId,
+				route.params.date,
+			)
+			.then(({data, init, count, phone}) => {
+				setPhoneInput(phone ?? "");
+				setPhoneNumber(phone);
+				if (init <= 0) {
+					Alert.alert(getStr("failure"), getStr("sportsBookUnavailable"));
+					setResources([]);
+				} else if (count === 0) {
+					Alert.alert(
+						getStr("failure"),
+						getStr("sportsBookRestricted").replace("%d", String(init)),
+					);
+					setResources([]);
+				} else if (phone === undefined) {
+					Alert.alert(getStr("sportsBookPhoneRequired"));
+					setResources([]);
+				} else {
+					const result: {[key: string]: TimePeriod} = {};
+					for (let {
+						timeSession,
+						locked,
+						userType,
+						resId,
+						fieldName,
+						cost,
+					} of data) {
+						if (result[timeSession] === undefined) {
+							result[timeSession] = {
+								description: timeSession,
+								total: 0,
+								availableFields: [],
+							};
 						}
-						return Object.values(result).reverse();
+						++result[timeSession].total;
+						if (locked !== true && userType === undefined) {
+							result[timeSession].availableFields.push({
+								id: resId,
+								name: fieldName,
+								cost: cost ?? 0,
+							});
+						}
 					}
-				}),
-		(item, refresh, props, {}, index, total) => (
-			<View
-				style={{
-					borderTopColor: "lightgrey",
-					borderTopWidth: 1,
-					borderBottomColor: "lightgrey",
-					borderBottomWidth: index === total - 1 ? 1 : 0,
-				}}>
-				<TouchableOpacity
-					onPress={() =>
-						navigation.navigate("SportsSelect", {
-							info: route.params.info,
-							date: route.params.date,
-							phone: phoneNumber ?? "",
-							availableFields: item.availableFields,
-						})
-					}
-					disabled={item.availableFields.length === 0}>
-					<Text
-						style={{
-							padding: 10,
-							textDecorationLine:
-								item.availableFields.length === 0 ? "line-through" : "none",
-							color:
-								item.availableFields.length === 0 ? "lightgrey" : colors.text,
-						}}>
-						{item.description} ({item.availableFields.length}/{item.total})
-					</Text>
-				</TouchableOpacity>
-			</View>
-		),
-		({description}) => description,
-	);
+					setResources(Object.values(result));
+				}
+			})
+			.catch(NetworkRetry)
+			.then(() => {
+				setRefreshing(false);
+			});
+	};
+
+	useEffect(() => {
+		refresh();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<>
@@ -164,7 +145,51 @@ export const SportsDetailScreen = ({
 					<Text style={{color: "black"}}>{getStr("confirm")}</Text>
 				</TouchableOpacity>
 			</View>
-			<RefreshList />
+			<FlatList
+				style={{flex: 1}}
+				refreshControl={
+					<RefreshControl
+						refreshing={refreshing}
+						onRefresh={refresh}
+						colors={[colors.accent]}
+					/>
+				}
+				data={resources}
+				renderItem={({item, index}) => (
+					<View
+						style={{
+							borderTopColor: "lightgrey",
+							borderTopWidth: 1,
+							borderBottomColor: "lightgrey",
+							borderBottomWidth: index === resources.length - 1 ? 1 : 0,
+						}}>
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate("SportsSelect", {
+									info: route.params.info,
+									date: route.params.date,
+									phone: phoneNumber ?? "",
+									availableFields: item.availableFields,
+								})
+							}
+							disabled={item.availableFields.length === 0}>
+							<Text
+								style={{
+									padding: 10,
+									textDecorationLine:
+										item.availableFields.length === 0 ? "line-through" : "none",
+									color:
+										item.availableFields.length === 0
+											? "lightgrey"
+											: colors.text,
+								}}>
+								{item.description} ({item.availableFields.length}/{item.total})
+							</Text>
+						</TouchableOpacity>
+					</View>
+				)}
+				keyExtractor={({description}) => description}
+			/>
 		</>
 	);
 };
