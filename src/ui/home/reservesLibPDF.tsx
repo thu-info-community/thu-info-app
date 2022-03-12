794ce7d2bbe632947e9373669d2ed80fe3de893e
UNIDY2002 <UNIDY2002@outlook.com>
2022-03-12 18:04:23 +0800

    feat: add a progress indicator for reserves lib download

--- src/ui/home/reservesLibPDF.tsx
+++ src/ui/home/reservesLibPDF.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {HomeNav, ReservesLibPDFProp} from "./homeStack";
-import {Dimensions, StyleSheet, View} from "react-native";
+import {Dimensions, StyleSheet, Text, View} from "react-native";
 import Pdf from "react-native-pdf";
 import {helper} from "../../redux/store";
+import RNFS from "react-native-fs";
 
 export const ReservesLibPDFScreen = ({
 	route: {
@@ -13,21 +14,41 @@ export const ReservesLibPDFScreen = ({
 	route: ReservesLibPDFProp;
 }) => {
 	const [content, setContent] = useState<string>();
+	const [total, setTotal] = useState(0);
+	const [done, setDone] = useState(0);
+	const [downloading, setDownloading] = useState(true);
 
 	useEffect(() => {
 		helper.getReservesLibBookDetail(book.bookId).then((r) => {
 			if (r) {
-				helper.reservesLibDownloadChapters(r.chapters).then((pdf) => {
-					setContent(
-						pdf.output("datauristring").replace("filename=generated.pdf;", ""),
-					);
-				});
+				helper
+					.reservesLibDownloadChapters(r.chapters, (t, c) => {
+						setTotal(t);
+						setDone(c);
+					})
+					.then((pdf) => {
+						setDownloading(false);
+						const base64 = pdf
+							.output("datauristring")
+							.replace("filename=generated.pdf;", "");
+						setContent(base64);
+						const filename = `[${book.bookId}] ${book.title} - ${book.author}.pdf`;
+						const path = RNFS.DownloadDirectoryPath + "/" + filename;
+						const saveData = base64.substring(base64.indexOf("base64,") + 7);
+						return RNFS.writeFile(path, saveData, "base64");
+					});
 			}
 		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [book.bookId]);
 
 	return (
 		<View style={styles.container}>
+			{content === undefined && (
+				<Text>
+					{downloading ? "下载中，请勿离开本页面！" : "转码中"}({done}/{total})
+				</Text>
+			)}
 			{content !== undefined && (
 				<Pdf style={styles.pdf} source={{uri: content}} />
 			)}
