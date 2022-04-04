b4a35df2c1a9536bdc7449a02edffcc9fe76163f
EveElseIf <EveElseIf@outlook.com>
2022-04-05 03:34:27 +0800

    update web api, remove lean cloud usage

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ polyfillURL();
 polyfillCrypto();
 import {name} from "./app.json";
 import {App} from "./src/App";
-import AV from "leancloud-storage/core";
-import * as adapters from "@leancloud/platform-adapters-react-native";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 
@@ -26,18 +24,6 @@ require("moment/locale/zh-cn");
 moment.locale("zh-cn");
 
 dayjs.extend(customParseFormat);
-
-// 由于代（ren）码（bi）开（jiao）源（lan），我决定公开LeanCloud相应密钥。
-// 我也希望除我以外的任何人不要使用以下信息访问相关数据库。
-// （况且目前看即使访问，能看到的也只有bug report和api统计）
-
-AV.setAdapters(adapters);
-// noinspection SpellCheckingInspection
-AV.init({
-	appId: "xbf9yBV8zr2zMwRwOjGLXNNz-gzGzoHsz",
-	appKey: "cxQfkadtrY7iQSl7HI4rlrGx",
-	serverURL: "https://xbf9ybv8.lc-cn-n1-shared.com",
-});
 
 // Fix MIUI12 font problem: https://segmentfault.com/a/1190000023622085
 
