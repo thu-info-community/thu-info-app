5b8d9023af8afe76b04b2b24873b3d0e886c5771
UNIDY2002 <UNIDY2002@outlook.com>
2022-05-07 15:33:26 +0800

    chore: ignore logbox message regarding ViewPropTypes deprecation

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ Text.render = function (...args) {
 	});
 };
 
-LogBox.ignoreLogs(["[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!"]);
+LogBox.ignoreLogs(["ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'."]);
 
 AppRegistry.registerComponent(name, () => App);
