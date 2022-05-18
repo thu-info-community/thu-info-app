12f9d47c8e14b7e67fd3bc007865b0bf5ef1875f
UNIDY2002 <UNIDY2002@outlook.com>
2022-05-18 22:02:35 +0800

    chore: remove unused constants

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,11 +1,3 @@
-export const UPDATE_URL_ANDROID =
-	"https://api.github.com/repos/UNIDY2002/THUInfo/releases/latest";
-export const UPDATE_URL_IOS =
-	"https://itunes.apple.com/lookup?bundleId=org.reactjs.native.example.thu-info";
-export const ANDROID_APP_CENTER_URL =
-	"https://install.appcenter.ms/users/unidy/apps/thuinfo/distribution_groups/cd";
-export const IOS_APP_STORE_URL =
-	"https://apps.apple.com/cn/app/thu-info/id1533968428";
 export const GITHUB_REPO_URL = "https://github.com/UNIDY2002/THUInfo";
 export const TUNA_BASE_URL =
 	"https://mirrors.tuna.tsinghua.edu.cn/github-release/UNIDY2002/THUInfo/";
