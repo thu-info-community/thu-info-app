5e89aee1c4bc342865562cf7f0b10387b863e06d
UNIDY2002 <UNIDY2002@outlook.com>
2022-05-20 09:58:35 +0800

    feat: remove news cache from the APP

--- src/redux/actions/cache.ts
+++
@@ -1,8 +0,0 @@
-import {ADD_NEWS_CACHE} from "../constants";
-import {NewsCache} from "../states/cache";
-import {ActionType, createAction} from "typesafe-actions";
-
-export const addNewsCacheAction = createAction(ADD_NEWS_CACHE)<NewsCache>();
-
-const cacheAction = {addNewsCacheAction};
-export type CacheAction = ActionType<typeof cacheAction>;
