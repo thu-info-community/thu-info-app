0249edd156f4da95c6e4c16149a5c5e183c77bfa
84634E1A607A <ajax-dong@outlook.com>
2022-09-30 20:15:39 +0800

    Added a scroll view for settings page

--- src/ui/settings/settings.tsx
+++ src/ui/settings/settings.tsx
@@ -8,6 +8,7 @@ import {
 	TouchableOpacity,
 	useColorScheme,
 	View,
+	ScrollView,
 } from "react-native";
 import Snackbar from "react-native-snackbar";
 import {NetworkRetry} from "../../components/easySnackbars";
@@ -36,135 +37,142 @@ export const SettingsScreen = ({navigation}: {navigation: RootNav}) => {
 
 	const [forceLoginDisabled, setForceLoginDisabled] = useState(false);
 	return (
-		<View style={{flex: 1, padding: 12}} key={darkModeHook}>
-			<RoundedView style={style.rounded}>
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("Account")}>
-					<Text style={style.text}>{getStr("accountAndSecurity")}</Text>
-					<IconRight height={20} width={20} />
-				</TouchableOpacity>
-			</RoundedView>
-			<RoundedView style={style.rounded}>
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("FunctionManagement")}>
-					<Text style={style.text}>{getStr("functionManagement")}</Text>
-					<IconRight height={20} width={20} />
-				</TouchableOpacity>
-				<View style={style.separator} />
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("General")}>
-					<Text style={style.text}>{getStr("general")}</Text>
-					<IconRight height={20} width={20} />
-				</TouchableOpacity>
-			</RoundedView>
-			<RoundedView style={style.rounded}>
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("Privacy")}>
-					<Text style={style.text}>{getStr("privacy")}</Text>
-					<IconRight height={20} width={20} />
-				</TouchableOpacity>
-				<View style={style.separator} />
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("HelpAndFeedback")}>
-					<Text style={style.text}>{getStr("helpAndFeedback")}</Text>
-					<IconRight height={20} width={20} />
-				</TouchableOpacity>
-				<View style={style.separator} />
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => navigation.navigate("About")}>
-					<Text style={style.text}>{getStr("aboutApp")}</Text>
-					<View style={{flexDirection: "row", alignItems: "center"}}>
-						<Text style={style.version}>
-							{getStr("version")} {VersionNumber.appVersion}
-						</Text>
+		<ScrollView>
+			<View style={{flex: 1, padding: 12}} key={darkModeHook}>
+				<RoundedView style={style.rounded}>
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("Account")}>
+						<Text style={style.text}>{getStr("accountAndSecurity")}</Text>
 						<IconRight height={20} width={20} />
-					</View>
-				</TouchableOpacity>
-			</RoundedView>
-			<Text style={{marginLeft: 8, marginTop: 12, color: theme.colors.fontB2}}>
-				{getStr("betaVersion")}
-			</Text>
-			<View style={{flex: 1}} />
-			<RoundedView style={style.rounded}>
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={async () => {
-						setForceLoginDisabled(true);
-						try {
-							Snackbar.show({
-								text: getStr("processing"),
-								duration: Snackbar.LENGTH_SHORT,
-							});
+					</TouchableOpacity>
+				</RoundedView>
+				<RoundedView style={style.rounded}>
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("FunctionManagement")}>
+						<Text style={style.text}>{getStr("functionManagement")}</Text>
+						<IconRight height={20} width={20} />
+					</TouchableOpacity>
+					<View style={style.separator} />
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("General")}>
+						<Text style={style.text}>{getStr("general")}</Text>
+						<IconRight height={20} width={20} />
+					</TouchableOpacity>
+				</RoundedView>
+				<RoundedView style={style.rounded}>
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("Privacy")}>
+						<Text style={style.text}>{getStr("privacy")}</Text>
+						<IconRight height={20} width={20} />
+					</TouchableOpacity>
+					<View style={style.separator} />
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("HelpAndFeedback")}>
+						<Text style={style.text}>{getStr("helpAndFeedback")}</Text>
+						<IconRight height={20} width={20} />
+					</TouchableOpacity>
+					<View style={style.separator} />
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => navigation.navigate("About")}>
+						<Text style={style.text}>{getStr("aboutApp")}</Text>
+						<View style={{flexDirection: "row", alignItems: "center"}}>
+							<Text style={style.version}>
+								{getStr("version")} {VersionNumber.appVersion}
+							</Text>
+							<IconRight height={20} width={20} />
+						</View>
+					</TouchableOpacity>
+				</RoundedView>
+				<Text
+					style={{marginLeft: 8, marginTop: 12, color: theme.colors.fontB2}}>
+					{getStr("betaVersion")}
+				</Text>
+				<View style={{flex: 1}} />
+				<RoundedView style={style.rounded}>
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={async () => {
+							setForceLoginDisabled(true);
 							try {
-								await helper.logout();
-							} catch (e) {}
-							await helper.login({});
-							helper.getCalendar().then((c) => {
-								store.dispatch(setCalendarConfigAction(c));
-							});
-							Snackbar.show({
-								text: getStr("success"),
-								duration: Snackbar.LENGTH_SHORT,
-							});
-						} catch (e) {
-							NetworkRetry();
-						}
-						setForceLoginDisabled(false);
-					}}
-					disabled={forceLoginDisabled}>
-					<Text style={[style.text, {textAlign: "center", flex: 1}]}>
-						{getStr("forceLogin")}
-					</Text>
-				</TouchableOpacity>
-			</RoundedView>
-			<RoundedView style={style.rounded}>
-				<TouchableOpacity
-					style={style.touchable}
-					onPress={() => {
-						Alert.alert(getStr("logout"), getStr("confirmLogout"), [
-							{text: getStr("cancel")},
-							{
-								text: getStr("no"),
-								onPress: () => {
-									helper
-										.logout()
-										.then(() => console.log("Successfully logged out."));
-									store.dispatch(doLogoutAction());
+								Snackbar.show({
+									text: getStr("processing"),
+									duration: Snackbar.LENGTH_SHORT,
+								});
+								try {
+									await helper.logout();
+								} catch (e) {}
+								await helper.login({});
+								helper.getCalendar().then((c) => {
+									store.dispatch(setCalendarConfigAction(c));
+								});
+								Snackbar.show({
+									text: getStr("success"),
+									duration: Snackbar.LENGTH_SHORT,
+								});
+							} catch (e) {
+								NetworkRetry();
+							}
+							setForceLoginDisabled(false);
+						}}
+						disabled={forceLoginDisabled}>
+						<Text style={[style.text, {textAlign: "center", flex: 1}]}>
+							{getStr("forceLogin")}
+						</Text>
+					</TouchableOpacity>
+				</RoundedView>
+				<RoundedView style={style.rounded}>
+					<TouchableOpacity
+						style={style.touchable}
+						onPress={() => {
+							Alert.alert(getStr("logout"), getStr("confirmLogout"), [
+								{text: getStr("cancel")},
+								{
+									text: getStr("no"),
+									onPress: () => {
+										helper
+											.logout()
+											.then(() => console.log("Successfully logged out."));
+										store.dispatch(doLogoutAction());
+									},
 								},
-							},
-							{
-								text: getStr("yes"),
-								onPress: () => {
-									helper
-										.logout()
-										.then(() => console.log("Successfully logged out."));
-									store.dispatch(doLogoutAction());
-									store.dispatch(setDormPasswordAction(""));
-									store.dispatch(scheduleClearAction());
-									store.dispatch(configSet("emailName", ""));
-									store.dispatch(setActiveLibBookRecordAction([]));
-									store.dispatch(setActiveSportsReservationRecordAction([]));
+								{
+									text: getStr("yes"),
+									onPress: () => {
+										helper
+											.logout()
+											.then(() => console.log("Successfully logged out."));
+										store.dispatch(doLogoutAction());
+										store.dispatch(setDormPasswordAction(""));
+										store.dispatch(scheduleClearAction());
+										store.dispatch(configSet("emailName", ""));
+										store.dispatch(setActiveLibBookRecordAction([]));
+										store.dispatch(setActiveSportsReservationRecordAction([]));
+									},
 								},
-							},
-						]);
-					}}>
-					<Text
-						style={[
-							style.text,
-							{textAlign: "center", flex: 1, color: theme.colors.statusWarning},
-						]}>
-						{getStr("logout")}
-					</Text>
-				</TouchableOpacity>
-			</RoundedView>
-			<View style={{height: 80}} />
-		</View>
+							]);
+						}}>
+						<Text
+							style={[
+								style.text,
+								{
+									textAlign: "center",
+									flex: 1,
+									color: theme.colors.statusWarning,
+								},
+							]}>
+							{getStr("logout")}
+						</Text>
+					</TouchableOpacity>
+				</RoundedView>
+				<View style={{height: 80}} />
+			</View>
+		</ScrollView>
 	);
 };
 
