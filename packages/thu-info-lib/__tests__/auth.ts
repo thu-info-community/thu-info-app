424a2ca1e6260644be17abe2164fc4f0d3aa5014
UNIDY2002 <UNIDY2002@outlook.com>
2022-10-12 15:40:39 +0800

    fix: fix test error

--- packages/thu-info-lib/__tests__/auth.ts
+++ packages/thu-info-lib/__tests__/auth.ts
@@ -17,8 +17,8 @@ try {
 it("should enter mocked account", async () => {
     const helper = new InfoHelper();
     await helper.login({userId: "8888", password: "8888"});
-    await helper.logout();
     expect(helper.mocked()).toEqual(true);
+    await helper.logout();
 }, 60000);
 
 it("should login successfully.", async () => {
