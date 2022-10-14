5dfcc8a47d83786c46345b240739a5b4da50467e
UNIDY2002 <UNIDY2002@outlook.com>
2022-10-14 11:27:51 +0800

    fix: avoid showing `undefined` in error messages

--- src/components/easySnackbars.ts
+++ src/components/easySnackbars.ts
@@ -3,6 +3,6 @@ import {getStr} from "../utils/i18n";
 
 export const NetworkRetry = (e?: any) =>
 	Snackbar.show({
-		text: getStr("networkRetry") + e?.message,
+		text: getStr("networkRetry") + (e?.message ?? ""),
 		duration: Snackbar.LENGTH_SHORT,
 	});
