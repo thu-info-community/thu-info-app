9de049edc248b887da72667d101e5d993168fb8a
UNIDY2002 <UNIDY2002@outlook.com>
2022-10-23 10:09:57 +0800

    fix: reduce secondary item button size
    Fix #309

--- src/components/home/secondaryItems.tsx
+++ src/components/home/secondaryItems.tsx
@@ -27,7 +27,7 @@ export const SecondaryItem = ({
 	const themeName = useColorScheme();
 	const style = styles(themeName);
 	const windowWidth = Dimensions.get("window").width;
-	const viewSize = (windowWidth - 32) / 2;
+	const viewSize = (windowWidth - 32) / 2 - 1;
 	const dispatch = useDispatch();
 	return (
 		<TouchableOpacity
