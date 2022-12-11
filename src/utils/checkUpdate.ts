05bb7c78acbe83f72ee06e29234e653e2c6d88d0
UNIDY2002 <UNIDY2002@outlook.com>
2022-12-11 20:20:00 +0800

    chore: switch from stable to beta
    (cherry picked from commit 931bc067b831515bb36884b8080847a71245a39a)

--- src/utils/checkUpdate.ts
+++ src/utils/checkUpdate.ts
@@ -5,13 +5,15 @@ import {getStr} from "./i18n";
 import {configSet} from "../redux/slices/config";
 import Snackbar from "react-native-snackbar";
 import VersionNumber from "react-native-version-number";
-import {gt, lt} from "semver";
+import {gte, lt} from "semver";
 import {NetworkRetry} from "../components/easySnackbars";
 import {TUNA_BASE_URL, TUNA_LATEST_URL} from "../constants/strings";
 
 export const checkUpdate = (force: boolean = false) => {
 	getUpdateInfo()
-		.then((r) => r.filter((it) => gt(it.versionName, VersionNumber.appVersion)))
+		.then((r) =>
+			r.filter((it) => gte(it.versionName, VersionNumber.appVersion)),
+		)
 		.then((r) => {
 			if (
 				r.length &&
