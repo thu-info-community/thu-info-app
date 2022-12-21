02ad3f3dbd458d0217ae5c1432a3bfa0274baa1a
UNIDY2002 <UNIDY2002@outlook.com>
2022-12-21 15:55:44 +0800

    Revert "chore: switch from stable to beta"
    This reverts commit 931bc067b831515bb36884b8080847a71245a39a.

--- src/ui/settings/settings.tsx
+++ src/ui/settings/settings.tsx
@@ -90,7 +90,7 @@ export const SettingsScreen = ({navigation}: {navigation: RootNav}) => {
 						<Text style={style.text}>{getStr("aboutApp")}</Text>
 						<View style={{flexDirection: "row", alignItems: "center"}}>
 							<Text style={style.version}>
-								{getStr("version")} {VersionNumber.appVersion}-beta.4
+								{getStr("version")} {VersionNumber.appVersion}
 							</Text>
 							<IconRight height={20} width={20} />
 						</View>
