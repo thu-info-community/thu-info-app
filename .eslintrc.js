292fb91463744e796aa01c8e3197a802ad12d034
UNIDY2002 <UNIDY2002@outlook.com>
2023-01-04 17:11:14 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,5 @@ module.exports = {
 		"react/no-unstable-nested-components": ["off"],
 		"react-native/no-inline-styles": ["off"],
 		quotes: ["error", "double"],
-		// https://github.com/eslint/eslint/issues/13640
-		"no-shadow": ["off"],
-		"@typescript-eslint/no-shadow": [1],
 	},
 };
