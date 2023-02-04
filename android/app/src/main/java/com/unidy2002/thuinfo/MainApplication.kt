8cfbcb73dd232bf7d56a6aba3bba258880f8cddf
UNIDY2002 <UNIDY2002@outlook.com>
2023-02-04 13:46:20 +0800

    chore: java project cleanup

--- android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
+++ android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
@@ -3,7 +3,6 @@ package com.unidy2002.thuinfo
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
