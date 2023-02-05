0a5b8b246d9f70e0e927700a4ac66e06946c0285
UNIDY2002 <UNIDY2002@outlook.com>
2023-02-05 12:15:42 +0800

    chore: change dependency `@react-native-community/eslint-config` into `@react-native/eslint-config`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true,
-	extends: "@react-native-community",
+	extends: "@react-native",
 	rules: {
 		"react/react-in-jsx-scope": ["off"],
 		"react/no-unstable-nested-components": ["off"],
