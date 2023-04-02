b0ad7336c096d6c4a664ff18674dcd667ed79d34
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2023-04-03 06:55:18 +0800

    Make the slider more like a slider (#356)

--- src/redux/slices/config.ts
+++ src/redux/slices/config.ts
@@ -17,7 +17,7 @@ export interface ConfigState {
 	bx: boolean;
 	reportHidden: string[];
 	scheduleHeight: number;
-	scheduleHeightMode: 1 | 2 | 3 | undefined;
+	scheduleHeightMode: number | undefined;
 	lastBroadcast: number;
 	emailUnseen: number;
 	receiptTitle: ValidReceiptTypes | undefined;
