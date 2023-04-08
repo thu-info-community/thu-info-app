d32e2c8a929b8fbb1ef16b4c8c0c5b459ff6d706
EveElseIf <EveElseIf@outlook.com>
2023-04-08 23:50:30 +0800

    update network icons.

--- src/assets/icons/IconNetworkDetail.tsx
+++ src/assets/icons/IconNetworkDetail.tsx
@@ -2,43 +2,50 @@ import Svg, {Path, Rect} from "react-native-svg";
 import themes from "../themes/themes";
 import {useColorScheme} from "react-native";
 
-export default ({width, height}: {width?: number; height?: number}) => {
-	const themeName = useColorScheme();
-	const theme = themes(themeName);
-	return (
-		<Svg viewBox="0 0 656 656" width={width} height={height}>
-			<Path
-				d="m481.07,655.97v-174.92h65.59v-131.2h-196.79v131.2h65.59v174.92h-174.92v-174.92h65.59v-131.2H109.34v131.2h65.59v174.92H0v-174.92h65.59v-174.92h240.51v-131.2h-65.59V0h174.93v174.92h-65.59v131.2h240.52v174.92h65.59v174.92h-174.93.05Zm131.18-131.2"
-				fill={theme.colors.fontB1}
-			/>
-			<Rect
-				x="524.8"
-				y="524.77"
-				width="87.45"
-				height="87.45"
-				fill={theme.colors.mainTheme}
-			/>
-			<Rect
-				x="284.29"
-				y="524.77"
-				width="87.45"
-				height="87.45"
-				fill={theme.colors.mainTheme}
-			/>
-			<Rect
-				x="43.78"
-				y="524.77"
-				width="87.45"
-				height="87.45"
-				fill={theme.colors.mainTheme}
-			/>
-			<Rect
-				x="284.29"
-				y="43.75"
-				width="87.45"
-				height="87.45"
-				fill={theme.colors.mainTheme}
-			/>
-		</Svg>
-	);
+export default ({width, height}: { width?: number; height?: number }) => {
+    const themeName = useColorScheme();
+    const theme = themes(themeName);
+    return (
+        <Svg width={width} height={height} viewBox="0 0 48 48" fill="none">
+            <Rect x="4" y="34" width="8" height="8"
+                  fill={theme.colors.mainTheme}
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Rect x="8" y="6" width="32" height="12"
+                  fill={theme.colors.mainTheme}
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Path d="M24 34V18"
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Path d="M8 34V26H40V34"
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Rect x="36" y="34" width="8" height="8"
+                  fill={theme.colors.mainTheme}
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Rect x="20" y="34" width="8" height="8"
+                  fill={theme.colors.mainTheme}
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+            <Path d="M14 12H16"
+                  stroke={theme.colors.fontB1}
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"/>
+        </Svg>
+    );
 };
