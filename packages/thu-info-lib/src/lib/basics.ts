3c7f5e1a3d6718cc08504523f9651e498318c1e4
UNIDY2002 <UNIDY2002@outlook.com>
2023-06-28 12:49:35 +0800

    Fix: 2023 summer semester firstDay

--- packages/thu-info-lib/src/lib/basics.ts
+++ packages/thu-info-lib/src/lib/basics.ts
@@ -627,7 +627,7 @@ export const getCalendar = async (helper: InfoHelper): Promise<CalendarData> =>
         "",
         async () => {
             const {object} = await uFetch(`${CALENDAR_URL}?_csrf=${await getCsrfToken()}`).then(JSON.parse);
-            const firstDay = object.jyzdyt === "2022-09-13" ? "2022-09-12" : object.jyzdyt;  // 难得一遇的周二开学
+            const firstDay = object.jyzdyt === "2023-06-27" ? "2023-06-26" : object.jyzdyt;  // 难得两遇的周二开学
             const semesterId = object.xnxq;
             const semesterCode = semesterId[semesterId.length - 1];
             const weekCount = semesterCode === "3" ? 12 : 18;
