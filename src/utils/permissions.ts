34eef07af27c8039fbc21980736d37187ff5e7aa
UNIDY2002 <UNIDY2002@outlook.com>
2023-07-07 10:39:47 +0800

    fix: android permission issue regarding `READ_MEDIA_IMAGES`

--- src/utils/permissions.ts
+++
@@ -1,5 +0,0 @@
-import {Permission, PermissionsAndroid} from "react-native";
-
-export const hasAndroidPermission = async (permission: Permission) =>
-	(await PermissionsAndroid.check(permission)) ||
-	(await PermissionsAndroid.request(permission)) === "granted";
--- src/utils/saveImg.ts
+++ src/utils/saveImg.ts
@@ -1,5 +1,5 @@
-import {PermissionsAndroid, Platform} from "react-native";
-import {hasAndroidPermission} from "./permissions";
+import {Platform} from "react-native";
+import {check, PERMISSIONS, request, RESULTS} from "react-native-permissions";
 import {CameraRoll} from "@react-native-camera-roll/camera-roll";
 import {getStr} from "./i18n";
 import Snackbar from "react-native-snackbar";
@@ -9,31 +9,44 @@ import {NetworkRetry} from "../components/easySnackbars";
 import CookieManager from "@react-native-cookies/cookies";
 
 export const saveImg = async (uri: string) => {
+	const permission =
+		Platform.OS === "android" && Platform.Version >= 33
+			? PERMISSIONS.ANDROID.READ_MEDIA_IMAGES
+			: PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE;
+	const checkPermissions = await check(permission);
+	if (checkPermissions === RESULTS.DENIED) {
+		const requestPermissions = await request(permission);
+		if (requestPermissions !== RESULTS.GRANTED) {
+			Snackbar.show({
+				text: getStr("permissionDenied"),
+				duration: Snackbar.LENGTH_SHORT,
+			});
+			return;
+		}
+	}
 	if (
-		Platform.OS === "android" &&
-		!(await hasAndroidPermission(
-			PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-		))
+		checkPermissions === RESULTS.BLOCKED ||
+		checkPermissions === RESULTS.LIMITED
 	) {
 		Snackbar.show({
 			text: getStr("permissionDenied"),
 			duration: Snackbar.LENGTH_SHORT,
 		});
-	} else {
-		CameraRoll.save(uri)
-			.then(() =>
-				Snackbar.show({
-					text: getStr("saveSucceed"),
-					duration: Snackbar.LENGTH_SHORT,
-				}),
-			)
-			.catch(() =>
-				Snackbar.show({
-					text: getStr("saveFailRetry"),
-					duration: Snackbar.LENGTH_SHORT,
-				}),
-			);
+		return;
 	}
+	CameraRoll.save(uri)
+		.then(() =>
+			Snackbar.show({
+				text: getStr("saveSucceed"),
+				duration: Snackbar.LENGTH_SHORT,
+			}),
+		)
+		.catch(() =>
+			Snackbar.show({
+				text: getStr("saveFailRetry"),
+				duration: Snackbar.LENGTH_SHORT,
+			}),
+		);
 };
 
 export const saveRemoteImg = async (url: string) => {
