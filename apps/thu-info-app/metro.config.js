67f1c1023a9770b01c029140b859f25d191b6675
UNIDY2002 <UNIDY2002@outlook.com>
2023-09-25 17:22:57 +0800

    fix: invisibility of stack header back icon

--- apps/thu-info-app/metro.config.js
+++ apps/thu-info-app/metro.config.js
@@ -21,6 +21,19 @@ const config = {
 		],
 		disableHierarchicalLookup: true,
 	},
+	server: {
+		enhanceMiddleware: (middleware) => {
+			return (req, res, next) => {
+				// When an asset is imported outside the project root, it has wrong path on Android
+				// So we fix the path to correct one
+				if (/\/assets\/.+\.png\?.+$/.test(req.url)) {
+					req.url = `/assets/../..${req.url}`;
+				}
+
+				return middleware(req, res, next);
+			};
+		},
+	},
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
