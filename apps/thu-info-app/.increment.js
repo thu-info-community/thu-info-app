87177a39db5c1c919b954013e933fd8196b94529
UNIDY2002 <UNIDY2002@outlook.com>
2023-11-03 11:13:14 +0800

    Fix script `increment` in `@thu-info/app`
    Relevant issues:
    - https://github.com/lerna/lerna/issues/1510
    - https://github.com/lerna/lerna/issues/2326

--- apps/thu-info-app/.increment.js
+++ apps/thu-info-app/.increment.js
@@ -2,4 +2,4 @@ const fs = require("fs");
 const pkg = require("./package.json");
 
 const pkgString = fs.readFileSync("./package.json").toString();
-fs.writeFileSync("./package.json", pkgString.replace(`"build": ${pkg.build}`, `"build": ${pkg.build + 1}`));
+fs.writeFileSync("./package.json", pkgString.replace(`"build": ${pkg.build}`, `"build": ${pkg.build + 1}`).replace(`"@thu-info/lib": "${pkg.dependencies["@thu-info/lib"]}"`, `"@thu-info/lib": "*"`));
