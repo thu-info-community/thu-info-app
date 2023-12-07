0fb90652a5cae10427e7ba2258b9bd8ccca8ec46
UNIDY2002 <UNIDY2002@outlook.com>
2023-12-07 11:00:23 +0800

    chore: post upgrade to react-native@0.73.0

--- apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
+++ apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
@@ -3,14 +3,16 @@ package com.unidy2002.thuinfo
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
-    private val mReactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
 
         override fun getPackages(): List<ReactPackage> {
           // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -26,7 +28,8 @@ class MainApplication : Application(), ReactApplication {
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
 
-    override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
     override fun onCreate() {
         super.onCreate()
