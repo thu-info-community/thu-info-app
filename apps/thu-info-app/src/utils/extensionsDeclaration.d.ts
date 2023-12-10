b7a487b00c6cae608d92d2dc66ada368cc1453a4
UNIDY2002 <UNIDY2002@outlook.com>
2023-12-10 14:00:48 +0800

    fix: patch type declaration of package `redux-persist`
    https://github.com/rt2zz/redux-persist/issues/1459#issuecomment-1841271813

--- apps/thu-info-app/src/utils/extensionsDeclaration.d.ts
+++ apps/thu-info-app/src/utils/extensionsDeclaration.d.ts
@@ -9,3 +9,13 @@ declare global {
 		format(...args: any[]): string;
 	}
 }
+
+import type {Action, Reducer} from "redux";
+import type {PersistConfig, PersistState} from "redux-persist";
+
+declare module "redux-persist" {
+	export function persistReducer<S, A extends Action = Action, P = S>(
+		config: PersistConfig<S>,
+		baseReducer: Reducer<S, A, P>,
+	): Reducer<S & {_persist: PersistState}, A, P & {_persist?: PersistState}>;
+}
