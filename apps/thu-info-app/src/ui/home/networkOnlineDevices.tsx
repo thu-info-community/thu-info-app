6bc6bfd600d1b97eb8f16ec30fe0c9388182f0fc
Yunyang <sunnycloudyang@outlook.com>
2024-10-31 17:18:10 +0800

    test: test wrapper view

--- apps/thu-info-app/src/ui/home/networkOnlineDevices.tsx
+++ apps/thu-info-app/src/ui/home/networkOnlineDevices.tsx
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
-import {Device} from "@thu-info/lib/src/models/network/device";
-import {helper} from "../../redux/store";
+import { useEffect, useState } from "react";
+import { Device } from "@thu-info/lib/src/models/network/device";
+import { helper } from "../../redux/store";
 import Snackbar from "react-native-snackbar";
-import {getStr} from "../../utils/i18n";
-import {RefreshControl, ScrollView} from "react-native-gesture-handler";
+import { getStr } from "../../utils/i18n";
+import { RefreshControl, ScrollView } from "react-native-gesture-handler";
 import {
 	KeyboardAvoidingView,
 	// Platform,
@@ -15,15 +15,15 @@ import {
 	View,
 } from "react-native";
 import themes from "../../assets/themes/themes";
-import {RoundedView} from "../../components/views";
-import {useHeaderHeight} from "@react-navigation/elements";
-import {styles} from "../settings/settings";
+import { RoundedView } from "../../components/views";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { styles } from "../settings/settings";
 
-const DeviceCard = ({device, refresh}: {device: Device; refresh: Function}) => {
+const DeviceCard = ({ device, refresh }: { device: Device; refresh: Function }) => {
 	const themeName = useColorScheme();
-	const {colors} = themes(themeName);
+	const { colors } = themes(themeName);
 
-	const Item = ({left, right}: {left: string; right: string}) => {
+	const Item = ({ left, right }: { left: string; right: string }) => {
 		return (
 			<View
 				style={{
@@ -60,9 +60,9 @@ const DeviceCard = ({device, refresh}: {device: Device; refresh: Function}) => {
 	};
 
 	return (
-		<RoundedView style={{margin: 8, borderRadius: 24}}>
-			<View style={{marginHorizontal: 16}}>
-				<Text style={{fontSize: 16, marginVertical: 2, color: colors.text}}>
+		<RoundedView style={{ margin: 8, borderRadius: 24 }}>
+			<View style={{ marginHorizontal: 16 }}>
+				<Text style={{ fontSize: 16, marginVertical: 2, color: colors.text }}>
 					{device.ip4}
 				</Text>
 				<View
@@ -119,7 +119,7 @@ const DeviceCard = ({device, refresh}: {device: Device; refresh: Function}) => {
 
 export const NetworkOnlineDevicesScreen = () => {
 	const themeName = useColorScheme();
-	const {colors} = themes(themeName);
+	const { colors } = themes(themeName);
 
 	const [devices, setDevices] = useState<Device[]>([]);
 
@@ -145,135 +145,142 @@ export const NetworkOnlineDevicesScreen = () => {
 	};
 	useEffect(refresh, []);
 	return (
-		<KeyboardAvoidingView
-			style={{flex: 1}}
-			// behavior={Platform.OS === "ios" ? "padding" : "height"}
-			behavior="padding"
-			keyboardVerticalOffset={headerHeight}>
-			<View style={{flex: 1, flexDirection: "column"}}>
-				<ScrollView
-					refreshControl={
-						<RefreshControl
-							refreshing={refreshing}
-							onRefresh={refresh}
-							colors={[colors.accent]}
-						/>
-					}
-					style={{ padding: 8, flex: 1 }}
-				>
-					{devices.length > 0 ? (
-						devices.map((d) => (
-							<DeviceCard
-								refresh={refresh}
-								device={d}
-								key={d.ip4 + "@" + d.nasIp}
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: colors.themeBackground,
+			}}
+		>
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				// behavior={Platform.OS === "ios" ? "padding" : "height"}
+				behavior="padding"
+				keyboardVerticalOffset={headerHeight}>
+				<View style={{ flex: 1, flexDirection: "column" }}>
+					<ScrollView
+						refreshControl={
+							<RefreshControl
+								refreshing={refreshing}
+								onRefresh={refresh}
+								colors={[colors.accent]}
 							/>
-						))
-					) : (
-						<Text
-							style={{
-								textAlign: "center",
-								marginTop: 24,
-								color: colors.text,
-							}}>
-							{getStr("noOnlineDevice")}
-						</Text>
-					)}
-				</ScrollView>
-				<View
-					style={{
-						flexDirection: "row",
-						backgroundColor: colors.contentBackground,
-						columnGap: 16,
-					}}>
-					<View style={{flex: 1, flexDirection: "row", paddingStart: 16}}>
-						<Text
-							style={{
-								verticalAlign: "middle",
-								color: colors.text,
-								paddingBottom: 3,
-							}}>
-							{getStr("ipAddr")}
-						</Text>
-						<TextInput
-							style={{
-								flex: 1,
-								color: colors.text,
-								fontSize: 14,
-								paddingHorizontal: 16,
-							}}
-							onChangeText={setImportIp}
-							placeholder={"1.2.3.4"}
-							placeholderTextColor={colors.fontB2}
-						/>
-					</View>
-					<View style={{flexDirection: "row"}}>
-						<Text
-							style={{
-								verticalAlign: "middle",
-								color: colors.text,
-								paddingBottom: 2,
-							}}>
-							{getStr("internetAccess")}
-						</Text>
-						<Switch
-							value={internetAccess}
-							onValueChange={setInternetAccess}
-							thumbColor={internetAccess ? colors.themeDarkPurple : colors.themeDarkGrey}
-							trackColor={{true: colors.themePurple}}
-						/>
-					</View>
+						}
+						style={{ padding: 8 }}
+					>
+						{devices.length > 0 ? (
+							devices.map((d) => (
+								<DeviceCard
+									refresh={refresh}
+									device={d}
+									key={d.ip4 + "@" + d.nasIp}
+								/>
+							))
+						) : (
+							<Text
+								style={{
+									textAlign: "center",
+									marginTop: 24,
+									color: colors.text,
+								}}>
+								{getStr("noOnlineDevice")}
+							</Text>
+						)}
+					</ScrollView>
 					<View
 						style={{
 							flexDirection: "row",
-							alignItems: "center",
-							padding: 4,
-							backgroundColor: colors.themePurple,
+							backgroundColor: colors.contentBackground,
+							columnGap: 16,
 						}}>
-						<TouchableOpacity
-							style={{
-								padding: 8,
-							}}
-							onPress={() => {
-								if (importIp === "") {
-									Snackbar.show({
-										text: getStr("ipAddrEmpty"),
-										duration: Snackbar.LENGTH_SHORT,
-									});
-									return;
-								}
-
-								helper
-									.loginNetworkDevice(importIp, internetAccess)
-									.then((s) => {
-										Snackbar.show({
-											text: getStr("importSuccess") + " " + s,
-											duration: Snackbar.LENGTH_SHORT,
-										});
-									})
-									.then(refresh)
-									.catch((e) => {
-										let message = e?.message;
-
-										if (message === "ip_already_online_error") {
-											message = getStr("importAlreadyOnline");
-										} else if (!/E\d+:/g.test(message)) {
-											message = getStr("networkRetry") + message;
-										}
-
-										Snackbar.show({
-											text: message,
-											duration: Snackbar.LENGTH_SHORT,
-										});
-									});
-							}}>
-							<Text style={{color: "white", fontSize: 14, marginBottom: 4}}>
-								{getStr("proxyImport")}
+						<View style={{ flex: 1, flexDirection: "row", paddingStart: 16 }}>
+							<Text
+								style={{
+									verticalAlign: "middle",
+									color: colors.text,
+									paddingBottom: 3,
+								}}>
+								{getStr("ipAddr")}
 							</Text>
-						</TouchableOpacity>
+							<TextInput
+								style={{
+									flex: 1,
+									color: colors.text,
+									fontSize: 14,
+									paddingHorizontal: 16,
+								}}
+								onChangeText={setImportIp}
+								placeholder={"1.2.3.4"}
+								placeholderTextColor={colors.fontB2}
+							/>
+						</View>
+						<View style={{ flexDirection: "row" }}>
+							<Text
+								style={{
+									verticalAlign: "middle",
+									color: colors.text,
+									paddingBottom: 2,
+								}}>
+								{getStr("internetAccess")}
+							</Text>
+							<Switch
+								value={internetAccess}
+								onValueChange={setInternetAccess}
+								thumbColor={internetAccess ? colors.themeDarkPurple : colors.themeDarkGrey}
+								trackColor={{ true: colors.themePurple }}
+							/>
+						</View>
+						<View
+							style={{
+								flexDirection: "row",
+								alignItems: "center",
+								padding: 4,
+								backgroundColor: colors.themePurple,
+							}}>
+							<TouchableOpacity
+								style={{
+									padding: 8,
+								}}
+								onPress={() => {
+									if (importIp === "") {
+										Snackbar.show({
+											text: getStr("ipAddrEmpty"),
+											duration: Snackbar.LENGTH_SHORT,
+										});
+										return;
+									}
+
+									helper
+										.loginNetworkDevice(importIp, internetAccess)
+										.then((s) => {
+											Snackbar.show({
+												text: getStr("importSuccess") + " " + s,
+												duration: Snackbar.LENGTH_SHORT,
+											});
+										})
+										.then(refresh)
+										.catch((e) => {
+											let message = e?.message;
+
+											if (message === "ip_already_online_error") {
+												message = getStr("importAlreadyOnline");
+											} else if (!/E\d+:/g.test(message)) {
+												message = getStr("networkRetry") + message;
+											}
+
+											Snackbar.show({
+												text: message,
+												duration: Snackbar.LENGTH_SHORT,
+											});
+										});
+								}}>
+								<Text style={{ color: "white", fontSize: 14, marginBottom: 4 }}>
+									{getStr("proxyImport")}
+								</Text>
+							</TouchableOpacity>
+						</View>
 					</View>
 				</View>
-			</View>
-		</KeyboardAvoidingView>
+			</KeyboardAvoidingView>
+		</View>
 	);
 };
