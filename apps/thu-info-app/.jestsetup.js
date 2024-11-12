892cf82740d78fa89b71f2e44f2d0829050df55c
UNIDY2002 <UNIDY2002@outlook.com>
2024-11-12 20:54:07 +0800

    fix: remove unused dependency react-native-reanimated

--- apps/thu-info-app/.jestsetup.js
+++ apps/thu-info-app/.jestsetup.js
@@ -63,12 +63,6 @@ jest.mock('react-native-fs', () => ({
 	}),
 );
 
-jest.mock("react-native-reanimated", () => {
-	const Reanimated = require("react-native-reanimated/mock");
-	Reanimated.default.call = () => {};
-	return Reanimated;
-});
-
 jest.mock("react-native-view-shot", () => ({
 	RNViewShot: jest.fn().mockResolvedValue(),
 }));
