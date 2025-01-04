9c756582fcf6a2157384b952635f4157f8a241ca
Yunyang <sunnycloudyang@outlook.com>
2025-01-04 15:33:32 +0800

    fix: fix browser extension

--- packages/thu-info-lib/demo/index.ts
+++ packages/thu-info-lib/demo/index.ts
@@ -1,11 +1,11 @@
 import {InfoHelper} from "../src";
 
 // @ts-ignore
-browser.browserAction.onClicked.addListener(() => {
-    // @ts-ignore
-    browser.tabs.create({
-        url: "index.html",
-    });
-});
+// browser.browserAction.onClicked.addListener(() => {
+//     // @ts-ignore
+//     browser.tabs.create({
+//         url: "index.html",
+//     });
+// });
 
 (window as any).InfoHelper = {InfoHelper};
