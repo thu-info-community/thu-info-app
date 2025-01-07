a2cb8a2f2e8374a53f25bdcf1dbf1513e9a79c06
UNIDY2002 <UNIDY2002@outlook.com>
2025-01-07 11:13:18 +0800

    fix: i18n of `forceLogin`

--- apps/thu-info-app/src/assets/translations/en.ts
+++ apps/thu-info-app/src/assets/translations/en.ts
@@ -21,7 +21,7 @@ export default {
 	twoFactorTrust: "Do you want to mark this device as a trusted device?",
 	slogan: "Login once, roam anywhere",
 	loggingIn: "Logging In...",
-	forceLogin: "Force login",
+	forceLogin: "Reset WebVPN Session",
 	logout: "Logout",
 	confirmLogout: "Remove all data?",
 	yes: "Yes",
