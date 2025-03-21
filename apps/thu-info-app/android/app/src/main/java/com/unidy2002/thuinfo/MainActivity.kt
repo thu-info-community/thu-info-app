111821acd7f551b408d4c880d0fe9a2cef3a09c2
SunnyCloudYang <sunnycloudyang@outlook.com>
2025-03-22 03:39:13 +0800

    feat: let navbar's background transparent

--- apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainActivity.kt
+++ apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.unidy2002.thuinfo
 
 import android.os.Bundle
+import android.os.Build
+import android.graphics.Color
+import android.view.View
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -15,6 +18,14 @@ class MainActivity : ReactActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(null)
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+        and (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM)) {
+            window.setNavigationBarContrastEnforced(false)
+            window.setNavigationBarColor(Color.TRANSPARENT)
+            window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                )
+        }
     }
 
     /**
