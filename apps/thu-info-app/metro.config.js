47986373c123740189562dd898b6738c1980ad64
UNIDY <unidy2002@outlook.com>
2025-03-25 17:39:16 +0800

    internal(ios): switch to Swift

--- apps/thu-info-app/metro.config.js
+++ apps/thu-info-app/metro.config.js
@@ -11,7 +11,7 @@ const workspaceRoot = path.resolve(projectRoot, "../..");
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
 	watchFolders: [workspaceRoot],
