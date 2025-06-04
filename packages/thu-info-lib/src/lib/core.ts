thu-info-community/thu-info-app

commit aca216cc769d9c34008c86cda9c7ad5cc8ac7499

fix: Redirect to WebVPN via oauth. to solve in-campus CSRF error (#705)

packages/thu-info-lib/src/lib/core.ts
@@ -74,6 +74,19 @@ const parseUrl = (urlIn: string) => {
     return `https://webvpn.tsinghua.edu.cn/${protocolFull}/${HOST_MAP[host]}/${path}`;
 };
 
+const getWebVPNUrl = (urlIn: string): string => {
+    if (urlIn.search("oauth.tsinghua.edu.cn") !== -1) {
+        return urlIn;
+    }
+
+    const url = new URL(urlIn);
+    const scheme = url.protocol.replace(":", "");
+    const host = url.hostname;
+    const port = url.port || (scheme == "https" ? "443" : "80");
+    const uri = url.pathname + (url.search ? url.search : "") + (url.hash ? url.hash : "");
+    return `https://oauth.tsinghua.edu.cn/lb-auth/lbredirect?scheme=${scheme}&host=${host}&port=${port}&uri=${uri}`;
+};
+
 export const getCsrfToken = async () => {
     const cookie = await uFetch(GET_COOKIE_URL);
     const q = /XSRF-TOKEN=(.+?);/.exec(cookie + ";");
@@ -293,7 +306,7 @@ export const roam = async (helper: InfoHelper, policy: RoamingPolicy, payload: s
         if (policy === "id_website") {
             return response;
         }
-        const redirectUrl = cheerio.load(response)("a").attr()!.href;
+        const redirectUrl = getWebVPNUrl(cheerio.load(response)("a").attr()!.href);
 
         if (redirectUrl.includes(HOST_MAP["madmodel.cs"])) {
             const ticket = /ticket=(.+)/.exec(redirectUrl);

commit 298244aad3944140f3384cfd6b5da59b216cc949

fix: fix regression caused by aca216cc

packages/thu-info-lib/src/lib/core.ts
@@ -306,7 +306,10 @@ export const roam = async (helper: InfoHelper, policy: RoamingPolicy, payload: s
         if (policy === "id_website") {
             return response;
         }
-        const redirectUrl = getWebVPNUrl(cheerio.load(response)("a").attr()!.href);
+        let redirectUrl = cheerio.load(response)("a").attr()!.href;
+        if (policy !== "card") {
+            redirectUrl = getWebVPNUrl(redirectUrl);
+        }
 
         if (redirectUrl.includes(HOST_MAP["madmodel.cs"])) {
             const ticket = /ticket=(.+)/.exec(redirectUrl);