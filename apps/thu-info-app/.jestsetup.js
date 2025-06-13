5749c92ceb2338adc451cfcec45c1c795fe3189f
UNIDY <unidy2002@outlook.com>
2025-06-13 10:40:26 +0800

    chore: update .jestsetup.js

--- apps/thu-info-app/.jestsetup.js
+++ apps/thu-info-app/.jestsetup.js
@@ -1,3 +1,5 @@
+import "react-native/jest/setup"
+
 import "react-native-gesture-handler/jestSetup"
 
 import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
