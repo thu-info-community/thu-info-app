e660094a3e90615cf986712397437de8a1e28bd7
shshsh-sh <3490840244@qq.com>
2025-12-11 19:35:26 +0800

    Oops...

--- apps/thu-info-app/src/utils/washer.ts
+++ apps/thu-info-app/src/utils/washer.ts
@@ -1,7 +1,7 @@
 import washerMap from "../assets/washers/washers"
 
 const REMOVE_STRS = ["清华大学", "学生", "-", "公寓", "宿舍区",
-                     "公", "宿舍", "", "4g", "4G", "洗鞋机"];
+                     "公", "宿舍", " ", "4g", "4G", "洗鞋机"];
 
 export function getWasherLoaction(id: string) {
     let name = washerMap[id];
