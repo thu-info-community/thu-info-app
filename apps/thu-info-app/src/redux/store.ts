ed8c49857fab1d5efb5a9045fb673327d6335e4c
UNIDY <unidy2002@outlook.com>
2026-08-13 13:14:13 +0800

    chore: deprecate react-native-device-time-format

--- apps/thu-info-app/src/redux/store.ts
+++ apps/thu-info-app/src/redux/store.ts
@@ -18,7 +18,7 @@ import createTransform from "redux-persist/es/createTransform";
 import {credentialsReducer, CredentialsState} from "./slices/credentials";
 import {InfoHelper} from "@thu-info/lib";
 import CookieManager from "@react-native-cookies/cookies";
-import {is24HourFormat} from "react-native-device-time-format";
+import {uses24HourClock} from "react-native-localize";
 import {
 	Schedule,
 	ScheduleTime,
@@ -78,9 +78,7 @@ AppState.addEventListener("change", (state) => {
 			store.dispatch(configSet({key: "subFunctionUnlocked", value: false}));
 		}
 		if (Platform.OS === "android" || Platform.OS === "ios") {
-			is24HourFormat().then((result) => {
-				store.dispatch(configSet({key: "is24Hour", value: result}));
-			});
+			store.dispatch(configSet({key: "is24Hour", value: uses24HourClock()}));
 		}
 	} else {
 		store.dispatch(configSet({key: "exitTimestamp", value: Date.now()}));
