606350927f44127d45942f997c55f789dc3762d1
Ajax Dong <i@aajax.top>
2026-09-09 17:38:32 +0800

    fix: restore Android keyboard avoidance height behavior

--- apps/thu-info-app/src/ui/home/networkOnlineDevices.tsx
+++ apps/thu-info-app/src/ui/home/networkOnlineDevices.tsx
@@ -6,7 +6,7 @@ import { getStr } from "../../utils/i18n";
 import { GestureHandlerRootView, RefreshControl, ScrollView } from "react-native-gesture-handler";
 import {
 	KeyboardAvoidingView,
-	// Platform,
+	Platform,
 	Switch,
 	Text,
 	TextInput,
@@ -148,8 +148,7 @@ export const NetworkOnlineDevicesScreen = ({navigation}: {navigation: RootNav}) 
 	return (
 		<KeyboardAvoidingView
 			style={{ flex: 1 }}
-			// behavior={Platform.OS === "ios" ? "padding" : "height"}
-			behavior="padding"
+			behavior={Platform.OS === "android" ? "height" : "padding"}
 			keyboardVerticalOffset={headerHeight}>
 			<GestureHandlerRootView style={{ flex: 1, flexDirection: "column" }}>
 				<ScrollView
