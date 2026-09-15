4e60124fda4e0a3cf1e59e334d746dff4da1335b
Xun Sun <UNIDY2002@outlook.com>
2026-09-15 13:44:00 +0800

    fix(harmony): fix `getRedirectResponse` (#959)

--- packages/RTNNetworkUtils/src/specs/v2/NativeNetworkUtils.ts
+++ packages/RTNNetworkUtils/src/specs/v2/NativeNetworkUtils.ts
@@ -3,6 +3,10 @@ import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
     getRedirectLocation(url: string): Promise<string | null | undefined>;
+    getRedirectResponse(url: string, timeoutMs: number): Promise<{
+        status: number;
+        location: string | null;
+    }>;
 }
 
 export default TurboModuleRegistry.get<Spec>(
